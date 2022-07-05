2e3a5fddd6b0b47acb16782517254aad6c3b0383
= <cameron.norton@gmail.com>
2022-07-05 10:52:36 -0700

    Update auxiliary files.

--- customize.el
+++ customize.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("738c4838957c1884dfacbb6f4f783c54e87c4a6b31c336d6279fc1c2b2ee56c5" "a53f00556ab4c81a0618ab6589053d9e351312d37d9c9cf544e0c8edac2b63ab" "88deeaaab5a631834e6f83e8359b571cfcbf5a18b04990288c8569cc16ee0798" default))
+   '("864646646f61d732efdde358b070b58ff6d541291d04fefdc5af6c9581eaa4aa" "738c4838957c1884dfacbb6f4f783c54e87c4a6b31c336d6279fc1c2b2ee56c5" "a53f00556ab4c81a0618ab6589053d9e351312d37d9c9cf544e0c8edac2b63ab" "88deeaaab5a631834e6f83e8359b571cfcbf5a18b04990288c8569cc16ee0798" default))
  '(package-selected-packages
    '(neotree simple-modeline vertico use-package rust-mode markdown-mode magit lua-mode go-mode flycheck fennel-mode elpy elfeed eglot auctex))
  '(url-cookie-confirmation nil))
