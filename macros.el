6803fe5e7d93f3fb3f678419204497b36584d3c4
Cameron Norton <cameron.norton@gmail.com>
2022-11-16 00:58:29 -0800

    Add a macro to create a full-frame calc view.

--- macros.el
+++ macros.el
@@ -4,3 +4,5 @@
 (fset 'insert-javadoc-comment
    (kmacro-lambda-form [?/ ?* ?* ?* ?/ ?\C-b ?\C-b return return ?\C-p ?* ?\C-p tab ?\C-n tab ?\C-n tab ?\C-p ? ] 0 "%d"))
 
+(fset 'calc-full-window
+   (kmacro-lambda-form [?\C-x ?1 ?\M-x ?c ?a ?l ?c return ?\C-x ?o ?\C-x ?o ?\C-x ?0 ?\C-x ?o] 0 "%d"))
