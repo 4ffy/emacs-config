b4ff23f870577804e7f8973e1010f5d4aed3939e
Cameron Norton <cameron.norton@gmail.com>
2023-03-12 23:19:45 -0700

    Update customize.
     - New packages
     - Suppress Hyperbole compilation warnings.

--- customize.el
+++ customize.el
@@ -5,15 +5,15 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("864646646f61d732efdde358b070b58ff6d541291d04fefdc5af6c9581eaa4aa" "738c4838957c1884dfacbb6f4f783c54e87c4a6b31c336d6279fc1c2b2ee56c5" "a53f00556ab4c81a0618ab6589053d9e351312d37d9c9cf544e0c8edac2b63ab" "88deeaaab5a631834e6f83e8359b571cfcbf5a18b04990288c8569cc16ee0798" default))
- '(erc-hide-list '("JOIN" "NICK" "PART" "QUIT"))
  '(package-selected-packages
-   '(json-mode js2-mode company yasnippet-snippets yasnippet yaml-mode keepass-mode cuda-mode ess orderless org-roam esup use-package magit avy clojure-mode marginalia racket-mode rust-mode dmenu exwm link-hint diminish meow d-mode undo-tree cider exec-path-from-shell cmake-mode eshell-vterm vterm neotree simple-modeline vertico markdown-mode lua-mode go-mode flycheck fennel-mode elpy elfeed eglot auctex))
+   '(erc-hl-nicks ef-themes modus-themes json-mode js2-mode company yasnippet-snippets yasnippet yaml-mode keepass-mode cuda-mode ess orderless org-roam esup use-package magit avy clojure-mode marginalia racket-mode rust-mode dmenu exwm link-hint diminish meow d-mode undo-tree cider exec-path-from-shell cmake-mode eshell-vterm vterm neotree simple-modeline vertico markdown-mode lua-mode go-mode flycheck fennel-mode elpy elfeed eglot auctex))
  '(safe-local-variable-values
    '((eval add-hook 'before-save-hook 'time-stamp)
      (vc-prepare-patches-separately)
      (diff-add-log-use-relative-names . t)
      (vc-git-annotate-switches . "-w")))
- '(url-cookie-confirmation nil))
+ '(url-cookie-confirmation nil)
+ '(warning-suppress-types '((comp))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
