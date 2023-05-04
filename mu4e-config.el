9f0497c9c2c124f4b4a57b02d59936cca4656fc1
Cameron Norton <cameron.norton@gmail.com>
2023-05-04 11:39:36 -0700

    Rewrite mu4e-config to use several use-package statements.
    I was having a bit of trouble getting the display-buffer-alist hack to
    work without loading mu4e at startup. This seemed like a job that
    use-package could handle well. While I'm at it, I could rewrite the
    rest of the configuration to make use of use-package as well.

--- mu4e-config.el
+++ mu4e-config.el
@@ -2,47 +2,45 @@
 ;;; MU4E CONFIG
 ;;;=============================================================================
 
-(require 'smtpmail)
-(autoload 'mu4e "mu4e" "Start mu4e." t)
+;;; Basic mail settings
+(use-package emacs
+  :custom
+  (message-kill-buffer-on-exit t)
+  (starttls-use-gnutls t))
 
-;; User agent
-(setq-default mail-user-agent 'mu4e-user-agent)
+;;; SMTP settings
+(use-package smtpmail
+  :custom
+  (message-send-mail-function 'smtpmail-send-it)
+  (smtpmail-auth-credentials '(("smtp.gmail.com" 587 user-mail-address nil)))
+  (smtpmail-default-smtp-server "smtp.gmail.com")
+  (smtpmail-smtp-server "smtp.gmail.com")
+  (smtpmail-smtp-service 587)
+  (smtpmail-starttls-credentials '(("smtp.gmail.com" 587 nil nil))))
 
-;; Email folders
-(setq-default mu4e-drafts-folder "/[Gmail].Drafts"
-              mu4e-sent-folder   "/[Gmail].Sent Mail"
-              mu4e-trash-folder  "/[Gmail].Trash")
-
-;; Don't save messages to "Sent Mail" as Gmail already does this
-(setq-default mu4e-sent-messages-behavior 'delete)
-
-;; Quick shocuts to mail folders
-(setq-default mu4e-maildir-shortcuts
-              '((:maildir "/INBOX"             :key ?i)
-                (:maildir "/[Gmail].Drafts"    :key ?d)
-                (:maildir "/[Gmail].Sent Mail" :key ?s)
-                (:maildir "/[Gmail].Trash"     :key ?t)))
-
-;; Set up mail fetching
-(setq-default mu4e-get-mail-command "offlineimap")
-
-;; Sending mail
-(setq-default message-send-mail-function 'smtpmail-send-it
-              mu4e-compose-signature user-full-name
-              smtpmail-auth-credentials '(("smtp.gmail.com" 587 user-mail-address nil))
-              smtpmail-default-smtp-server "smtp.gmail.com"
-              smtpmail-smtp-server "smtp.gmail.com"
-              smtpmail-smtp-service 587
-              smtpmail-starttls-credentials '(("smtp.gmail.com" 587 nil nil))
-              starttls-use-gnutls t)
-
-;; Close buffer on message exit
-(setq-default message-kill-buffer-on-exit t)
-
-;; Don't use ido
-(setq-default mu4e-completing-read-function nil)
-
-;; Prevent mu4e from opening full-frame.
-(add-to-list 'display-buffer-alist
-             `(,(regexp-quote mu4e-main-buffer-name)
-               display-buffer-same-window))
+;;; Set up mu4e itself.
+(use-package mu4e
+  :defer t
+  :commands mu4e
+  :custom
+  ;; Set up mail folders.
+  (mu4e-drafts-folder "/[Gmail].Drafts")
+  (mu4e-sent-folder   "/[Gmail].Sent Mail")
+  (mu4e-trash-folder  "/[Gmail].Trash")
+  ;; Set up mail folder shortcuts.
+  (mu4e-maildir-shortcuts
+   '((:maildir "/INBOX"             :key ?i)
+     (:maildir "/[Gmail].Drafts"    :key ?d)
+     (:maildir "/[Gmail].Sent Mail" :key ?s)
+     (:maildir "/[Gmail].Trash"     :key ?t)))
+  ;; Other mu4e settings.
+  (mail-user-agent 'mu4e-user-agent)
+  (mu4e-completing-read-function 'completing-read)
+  (mu4e-compose-signature user-full-name)
+  (mu4e-get-mail-command "offlineimap")
+  (mu4e-sent-messages-behavior 'delete)
+  :config
+  ;; Open mu4e in same window.
+  (add-to-list 'display-buffer-alist
+               `(,(regexp-quote mu4e-main-buffer-name)
+                 (display-buffer-same-window))))
