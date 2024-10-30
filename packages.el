d650727525adcc5427d1e77ea04f2a734c9f0263
Cameron Norton <cameron.norton@gmail.com>
2024-10-29 17:44:38 -0700

    Remove casual-calc.
    Casual keeps having breaking changes requiring manual intervention on
    my part, and I don't want to deal with that for a menu that I have
    barely used.

--- packages.el
+++ packages.el
@@ -50,12 +50,6 @@
   (("C-c j" . avy-goto-word-1)
    ("C-c J" . avy-goto-char-2)))
 
-;; Casual (Calc transient)
-(use-package casual-calc
-  :ensure t
-  :pin melpa
-  :bind (:map calc-mode-map ("C-o" . 'casual-calc-tmenu)))
-
 ;; Clang Format (provided by system clang package)
 (use-package clang-format
   :when (and (equal system-type 'gnu/linux) (executable-find "clang"))
