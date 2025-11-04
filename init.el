5554872ee3de351c94e0f60b78a49a7d95225eb0
Cameron Norton <cameron.norton@gmail.com>
2025-11-04 13:24:58 -0800

    Enable winner-mode on startup.
    Neat little builtin package. Use default keybinds.

--- init.el
+++ init.el
@@ -107,6 +107,7 @@
 (display-time-mode 1)
 (repeat-mode 1)
 (tooltip-mode -1)
+(winner-mode 1)
 (add-to-list 'default-frame-alist '(menu-bar-lines . nil))
 (add-to-list 'default-frame-alist '(tool-bar-lines . nil))
 (add-to-list 'default-frame-alist '(vertical-scroll-bars . nil))
